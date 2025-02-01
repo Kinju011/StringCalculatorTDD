bd82ae6c2c6bc7bf056098b2216def246fc5c8dd
Kinjal Patel <kinju011@gmail.com>
2025-02-01 17:25:41 +0530

    support multiple delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,12 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.gsub("\n", ",").split(',').map(&:to_i).sum
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers.split("\n", 2).last
+    else
+      delimiter = ","
+    end
+      numbers.gsub("\n", delimiter).split(delimiter).map(&:to_i).sum
   end
 end
