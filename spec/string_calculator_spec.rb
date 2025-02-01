6b33f31d5d0b980d6c8ee8496b5c25f7668a5c98
Kinjal Patel <kinju011@gmail.com>
2025-02-01 16:52:35 +0530

    failing spec for multiple number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,12 @@ describe StringCalculator do
   it "returns 0 for an empty string" do
     expect(StringCalculator.new.add("")).to eq(0)
   end
+
+  it "returns the number itself when a single number is passed" do
+    expect(StringCalculator.new.add("5")).to eq(5)
+  end
+
+  it "returns the sum of two numbers separated by a comma" do
+    expect(StringCalculator.new.add("1,2")).to eq(3)
+  end
 end
