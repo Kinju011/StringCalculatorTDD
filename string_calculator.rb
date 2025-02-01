1e0958524b9d9553c52e01a694f0e06f860f8c92
Kinjal Patel <kinju011@gmail.com>
2025-02-01 21:08:35 +0530

    refactored the code

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,17 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
+    delimiter = extract_delimiters_and_numbers(numbers).first
+    numbers = extract_delimiters_and_numbers(numbers).last
+
+    negative_check = numbers.gsub("\n", delimiter.first).split(Regexp.union(delimiter)).map(&:to_i)
+
+    raise "negative numbers not allowed #{negative_check.select{|n| n < 0}.join(", ")}" if negative_check.min < 0
+
+    negative_check.reject {|n| n > 1000}.sum
+  end
+
+  def extract_delimiters_and_numbers(numbers)
     if numbers.start_with?("//")
       delimiter = numbers.split("\n").first.tr('[]', '').tr("//", "").split('')
       numbers = numbers.split("\n", 2).last
@@ -9,10 +20,6 @@ class StringCalculator
       delimiter = [","]
     end
 
-    negative_check = numbers.gsub("\n", delimiter.first).split(Regexp.union(delimiter)).map(&:to_i)
-
-    raise "negative numbers not allowed #{negative_check.select{|n| n < 0}.join(", ")}" if negative_check.min < 0
-
-    negative_check.reject {|n| n > 1000}.sum
+    return delimiter, numbers
   end
 end
