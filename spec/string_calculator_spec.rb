f7aa2e614bd18336d3b239451231523c639b170d
Kinjal Patel <kinju011@gmail.com>
2025-02-01 17:02:27 +0530

    support multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../string_calculator'
 
-describe StringCalculator do
+
+describe StringCalculator do	
   it "returns 0 for an empty string" do
     expect(StringCalculator.new.add("")).to eq(0)
   end
