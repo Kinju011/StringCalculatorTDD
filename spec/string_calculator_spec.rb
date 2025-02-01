d281cbbecd0892966b67ee865ad627f8d64c5d2e
Kinjal Patel <kinju011@gmail.com>
2025-02-01 17:07:08 +0530

    spec for newline between numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,4 +13,8 @@ describe StringCalculator do
   it "returns the sum of two numbers separated by a comma" do
     expect(StringCalculator.new.add("1,2")).to eq(3)
   end
+
+  it "returns the sum of two numbers separated by a comma" do
+    expect(StringCalculator.new.add("1\n2,3")).to eq(5)
+  end
 end
