2394b81d2cb62302f8aa4e731f903e0597a57c81
Kinjal Patel <kinju011@gmail.com>
2025-02-01 18:27:29 +0530

    code enhamcement

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,32 +1,34 @@
 require_relative '../string_calculator'
 
 
-describe StringCalculator do	
+describe StringCalculator do
+  let(:string_calaulator) { StringCalculator.new }
+  
   it "returns 0 for an empty string" do
-    expect(StringCalculator.new.add("")).to eq(0)
+    expect(string_calaulator.add("")).to eq(0)
   end
 
   it "returns the number itself when a single number is passed" do
-    expect(StringCalculator.new.add("5")).to eq(5)
+    expect(string_calaulator.add("5")).to eq(5)
   end
 
   it "returns the sum of two numbers separated by a comma" do
-    expect(StringCalculator.new.add("1,2")).to eq(3)
+    expect(string_calaulator.add("1,2")).to eq(3)
   end
 
   it "returns the sum of two numbers separated by a comma or newline" do
-    expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+    expect(string_calaulator.add("1\n2,3")).to eq(6)
   end
 
   it "should support different delimiters" do
-    expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+    expect(string_calaulator.add("//;\n1;2")).to eq(3)
   end
 
   it "should raise an error for negative numbers" do
-    expect{ StringCalculator.new.add("//;\n1;2;-3") }.to raise_exception(StandardError, "negative numbers not supported")
+    expect{ string_calaulator.add("//;\n1;2;-3") }.to raise_exception(StandardError, "negative numbers not supported")
   end
 
   it "should raise an error for negative numbers with multiple negative numbers" do
-    expect{ StringCalculator.new.add("//;\n1;2;-3;6;-4") }.to raise_exception(StandardError, "negative numbers not supported -3, -4")
+    expect{ string_calaulator.add("//;\n1;2;-3;6;-4") }.to raise_exception(StandardError, "negative numbers not supported -3, -4")
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -3,15 +3,16 @@ class StringCalculator
     return 0 if numbers.empty?
 
     if numbers.start_with?("//")
-      delimiter = numbers[2]
-      numbers = numbers.split("\n", 2).last
+      delimiter, numbers = numbers[2], numbers.split("\n", 2).last
     else
       delimiter = ","
     end
 
     negative_check = numbers.gsub("\n", delimiter).split(delimiter).map(&:to_i)
 
-    raise "negative numbers not supported #{negative_check.select{|n| n < 0}.join(", ")}" if negative_check.min < 0 && negative_check.select{|n| n < 0}.count > 1
+    if negative_check.min < 0 && negative_check.select{|n| n < 0}.count > 1
+      raise "negative numbers not supported #{negative_check.select{|n| n < 0}.join(", ")}"
+    end
 
     raise "negative numbers not supported" if negative_check.min < 0
 
