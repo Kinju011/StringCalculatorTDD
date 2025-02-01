0161ada1fb3e4f7b0e4f0741bbd760107743010c
Kinjal Patel <kinju011@gmail.com>
2025-02-01 20:49:56 +0530

    multiple delimiters spec

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -39,4 +39,8 @@ describe StringCalculator do
   it "should support delimiters of any length" do
     expect(string_calaulator.add("//[***]\n1***2***3")).to eq(6)
   end
+
+  it "should support multiple delimiters" do
+    expect(string_calaulator.add("//[*][%]\n1*2%3")).to eq(6)
+  end
 end
